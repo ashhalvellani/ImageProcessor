02f318a82c1368472e82ae07dd02b8143e8997e6
Ashhal Vellani <66086941+ashhalvellani@users.noreply.github.com>
2023-02-28 20:43:04 -0800

    Add files via upload

---
+++ cmpt120imageManip.py
@@ -0,0 +1,262 @@
+# CMPT 120 Yet Another Image Processer
+# Starter code for cmpt120imageManip.py
+# Author 1: Ashhal Vellani | D300 | 301459228 
+# Author 2: Muhammad Aman-Ul-Haq | D200 | 301469945 
+# Date: December 1, 2021
+# Description: Image Processor Final Project
+
+from cmpt120imageProjHelper import getBlackImage, rgb_to_hsv
+
+
+# Basic 1: Red Filter
+def redFilter(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    for row in range(height):
+        for col in range(width):
+            pixels[row][col][1] = 0
+            pixels[row][col][2] = 0
+
+    return pixels
+
+
+# Basic 2: Green Filter
+def greenFilter(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    for row in range(height):
+        for col in range(width):
+            pixels[row][col][0] = 0
+            pixels[row][col][2] = 0
+
+    return pixels
+
+
+# Basic 3: Blue Filter
+def blueFilter(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    for row in range(height):
+        for col in range(width):
+            pixels[row][col][0] = 0
+            pixels[row][col][1] = 0
+
+    return pixels
+
+
+# Basic 4: Sepia Filter
+def sepiaFilter(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    for row in range(height):
+        for col in range(width):
+            pixel = pixels[row][col]
+
+            red, green, blue = pixel[0], pixel[1], pixel[2]
+
+            sepiaRed = (red * 0.393) + (green * 0.769) + (blue * 0.189)
+            sepiaGreen = (red * 0.349) + (green * 0.686) + (blue * 0.168)
+            sepiaBlue = (red * 0.272) + (green * 0.534) + (blue * 0.131)
+
+            # Use min to convert sepia > 255 to 255
+            minSepiaRed = min(int(sepiaRed), 255)
+            minSepiaGreen = min(int(sepiaGreen), 255)
+            minSepiaBlue = min(int(sepiaBlue), 255)
+
+            pixels[row][col][0] = minSepiaRed
+            pixels[row][col][1] = minSepiaGreen
+            pixels[row][col][2] = minSepiaBlue
+
+    return pixels
+
+
+# Define scale up and scale down functions to use in warm and cold filters
+def scaleUp(value):
+    if value < 64:
+        scaledUpValue = value / (64 * 80)
+
+    elif 64 <= value and value < 128:
+        scaledUpValue = (value - 64) / (128 - 64) * (160 - 80) + 80
+
+    else:
+        scaledUpValue = (value - 128) / (255 - 128) * (255 - 160) + 160
+
+    return int(scaledUpValue)
+
+
+def scaleDown(value):
+    if value < 64:
+        scaledDownValue = value / (64 * 50)
+
+    elif 64 <= value and value < 128:
+        scaledDownValue = (value - 64) / (128 - 64) * (100 - 50) + 50
+
+    else:
+        scaledDownValue = (value - 128) / (255 - 128) * (255 - 100) + 100
+
+    return int(scaledDownValue)
+
+
+# Basic 5: Warm Filter
+def warmFilter(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    for row in range(height):
+        for col in range(width):
+            red = pixels[row][col][0]
+            blue = pixels[row][col][2]
+
+            warmRed = scaleUp(red)
+            warmBlue = scaleDown(blue)
+
+            pixels[row][col][0] = warmRed
+            pixels[row][col][2] = warmBlue
+
+    return pixels
+
+
+# Basic 6: Cold Filter
+def coldFilter(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    for row in range(height):
+        for col in range(width):
+            pixel = pixels[row][col]
+
+            red = pixel[0]
+            blue = pixel[2]
+
+            coldBlue = scaleUp(blue)
+            coldRed = scaleDown(red)
+
+            pixel[0] = coldRed
+            pixel[2] = coldBlue
+
+    return pixels
+
+
+# Advanced 1: Rotate Left
+def rotateLeft(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    result = getBlackImage(height, width)
+
+    for row in range(height):
+        for col in range(width):
+            result[-col][row] = pixels[row][col]
+
+    return result
+
+
+# Advanced 2: Rotate Right
+def rotateRight(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    result = getBlackImage(height, width)
+
+    for row in range(height):
+        for col in range(width):
+            result[col][row] = pixels[-row][col]
+
+    return result
+
+
+# Advanced 3: Double Size
+def doubleSize(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    result = getBlackImage(width * 2, height * 2)
+
+    for row in range(height):
+        for col in range(width):
+            pixel = pixels[row][col]
+
+            result[row * 2][col * 2] = pixel
+            result[(row * 2) + 1][col * 2] = pixel
+            result[row * 2][(col * 2) + 1] = pixel
+            result[(row * 2) + 1][(col * 2) + 1] = pixel
+
+    return result
+
+
+# Advanced 4: Half Size
+def halfSize(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+
+    result = getBlackImage(width // 2, height // 2)
+
+    for row in range(height // 2):
+        for col in range(width // 2):
+
+            # In every 2x2 block of pixels define each
+            firstPixel = pixels[row * 2][col * 2]
+            secondPixel = pixels[(row * 2) + 1][col * 2]
+            thirdPixel = pixels[row * 2][(col * 2) + 1]
+            fourthPixel = pixels[(row * 2) + 1][(col * 2) + 1]
+
+            red = firstPixel[0] + secondPixel[0] + thirdPixel[0] + fourthPixel[0]
+            green = firstPixel[1] + secondPixel[1] + thirdPixel[1] + fourthPixel[1]
+            blue = firstPixel[2] + secondPixel[2] + thirdPixel[2] + fourthPixel[2]
+
+            result[row][col][0] = red // 4
+            result[row][col][1] = green // 4
+            result[row][col][2] = blue // 4
+
+    return result
+
+
+# Advanced 5: Locate Fish
+def locateFish(pixels):
+    height = len(pixels)
+    width = len(pixels[0])
+    result = getBlackImage(width, height)
+
+    # record all instances of yellow
+    yellowRows = []
+    yellowCols = []
+
+    for row in range(height):
+        for col in range(width):
+
+            result[row][col] = pixels[row][col]
+            pixel = result[row][col]
+
+            # convert rgb values to hsv values
+            r,g,b = pixel[0],pixel[1],pixel[2]
+            h,s,v = rgb_to_hsv(r, g, b)
+
+            # find all instances of yellow pixels on fish and add to list
+            if (48 < h < 74) and s > 40 and v > 55:
+                yellowRows.append(row)
+                yellowCols.append(col)
+
+    # find the top most and bottom most row and col that it appears on
+    topRow, bottomRow = min(yellowRows), max(yellowRows)
+    topCol, bottomCol = min(yellowCols), max(yellowCols)
+
+    return topRow, bottomRow, topCol, bottomCol
+
+def makeGreenBox(pixels, topRow, bottomRow, topCol, bottomCol):
+    for row in range(topRow, bottomRow):
+        pixels[row][topCol] = [0, 255, 0]
+
+    for row in range(topRow, bottomRow):
+        pixels[row][bottomCol] = [0, 255, 0]
+
+    for row in pixels[topRow]:
+        for col in range(topCol, bottomCol):
+            pixels[topRow][col] = [0, 255, 0]
+
+    for row in pixels[bottomRow]:
+        for col in range(topCol, bottomCol):
+            pixels[bottomRow][col] = [0, 255, 0]
---
+++ main.py
@@ -0,0 +1,233 @@
+# CMPT 120 Yet Another Image Processer
+# Starter code for main.py
+# Author 1: Ashhal Vellani | D300 | 301459228 
+# Author 2: Muhammad Aman-Ul-Haq | D200 | 301469945 
+# Date: December 1, 2021
+# Description: Image Processor Final Project
+
+import cmpt120imageProjHelper
+import cmpt120imageManip
+import tkinter.filedialog
+import pygame
+
+pygame.init()
+
+# list of system options
+system = [
+    "Q: Quit",
+    "O: Open Image",
+    "S: Save Current Image",
+    "R: Reload Original Image",
+]
+
+# list of basic operation options
+basic = [
+    "1: Apply Red Filter",
+    "2: Apply Green Filter",
+    "3: Apply Blue Filter",
+    "4: Apply Sepia Filter",
+    "5: Apply Warm Filter",
+    "6: Apply Cold Filter",
+    "7: Switch to Advanced Fuctions",
+]
+
+# list of advanced operation options
+advanced = [
+    "1: Rotate Left",
+    "2: Rotate Right",
+    "3: Double Size",
+    "4: Half Size",
+    "5: Locate Fish",
+    "6: Switch to Basic Functions",
+]
+
+# a helper function that generates a list of strings to be displayed in the interface
+def generateMenu(state):
+    """
+    Input:  state - a dictionary containing the state values of the application
+    Returns: a list of strings, each element represets a line in the interface
+    """
+    menuString = ["Welcome to CMPT 120 Image Processer!"]
+    menuString.append("")  # an empty line
+    menuString.append("Choose the following options:")
+    menuString.append("")  # an empty line
+    menuString += system
+    menuString.append("")  # an empty line
+
+    # build the list differently depending on the mode attribute
+    if state["mode"] == "basic":
+        menuString.append("--Basic Mode--")
+        menuString += basic
+        menuString.append("")
+        menuString.append("Enter your choice (Q/O/S/R or 1-7)...")
+    elif state["mode"] == "advanced":
+        menuString.append("--Advanced Mode--")
+        menuString += advanced
+        menuString.append("")
+        menuString.append("Enter your choice (Q/O/S/R or 1-6)...")
+    else:
+        menuString.append("Error: Unknown mode!")
+
+    return menuString
+
+
+# a helper function that returns the result image as a result of the operation chosen by the user
+# it also updates the state values when necessary (e.g, the mode attribute if the user switches mode)
+def handleUserInput(state, img):
+    """
+    Input:  state - a dictionary containing the state values of the application
+            img - the 2d array of RGB values to be operated on
+    Returns: the 2d array of RGB vales of the result image of an operation chosen by the user
+    """
+    userInput = state["lastUserInput"].upper()
+    # handle the system functionalities
+    if userInput.isalpha():  # check if the input is an alphabet
+        print("Log: Doing system functionalities " + userInput)
+
+        if userInput == "Q":  # this case actually won't happen, it's here as an example
+            print("Log: Quitting...")
+
+        elif userInput == "O": # open image
+            print("Log: Opening Image...")
+            tkinter.Tk().withdraw()
+            global openFilename
+            openFilename = tkinter.filedialog.askopenfilename()
+            img = cmpt120imageProjHelper.getImage(openFilename)
+            cmpt120imageProjHelper.showInterface(img, "Image Opened",generateMenu(appStateValues))
+
+        elif userInput == "S": # save image
+            print("Log: Saving Current Image...")
+            tkinter.Tk().withdraw()
+            saveFilename = tkinter.filedialog.asksaveasfilename()
+            cmpt120imageProjHelper.saveImage(img, f"{saveFilename}.jpg")
+            cmpt120imageProjHelper.showInterface(img, "Image Saved",generateMenu(state))
+
+        elif userInput == "R": # reload image
+            print("Log: Reloading Orginal Image")
+            img = cmpt120imageProjHelper.getImage(openFilename)
+            cmpt120imageProjHelper.showInterface(img, "Image Reloaded",generateMenu(state))
+
+        else: # unrecognized user input
+            print("Log: Unrecognized user input: " + userInput) 
+            cmpt120imageProjHelper.showInterface(img, "Choose Q,O,S or R",generateMenu(state))
+
+    # or handle the manipulation functionalities based on which mode the application is in
+    elif userInput.isdigit():  # has to be a digit for manipulation options
+        print("Log: Doing manipulation functionalities " + userInput)
+
+        if state["mode"] == "basic":
+            if userInput == "1": # red
+                print("Log: Performing " + basic[int(userInput) - 1])
+                img = cmpt120imageManip.redFilter(img)
+                cmpt120imageProjHelper.showInterface(img, "Red Filter Applied",generateMenu(state))
+
+            elif userInput == "2": # green
+                print("Log: Performing " + basic[int(userInput) - 1])
+                img = cmpt120imageManip.greenFilter(img)
+                cmpt120imageProjHelper.showInterface(img,"Green Filter Applied",generateMenu(state))
+
+            elif userInput == "3": # blue
+                print("Log: Performing " + basic[int(userInput) - 1])
+                img = cmpt120imageManip.blueFilter(img)
+                cmpt120imageProjHelper.showInterface(img,"Blue Filter Applied",generateMenu(state))
+
+            elif userInput == "4": # sepia
+                print("Log: Performing " + basic[int(userInput) - 1])
+                img = cmpt120imageManip.sepiaFilter(img)
+                cmpt120imageProjHelper.showInterface(img,"Sepia Filter Applied",generateMenu(state))
+
+            elif userInput == "5": # warm
+                print("Log: Performing " + basic[int(userInput) - 1])
+                img = cmpt120imageManip.warmFilter(img)
+                cmpt120imageProjHelper.showInterface(img,"Warm Filter Applied",generateMenu(state))
+
+            elif userInput == "6": # cold
+                print("Log: Performing " + basic[int(userInput) - 1])
+                img = cmpt120imageManip.coldFilter(img)
+                cmpt120imageProjHelper.showInterface(img,"Cold Filter Applied",generateMenu(state))
+
+            elif userInput == "7": # switch to advanced
+                print("Log: Performing " + basic[int(userInput) - 1])
+                state["mode"] = "advanced"
+                cmpt120imageProjHelper.showInterface(img, "Advanced Mode ",generateMenu(state))
+
+            else: # unrecognized user input
+                print("Log: Unrecognized user input: " + userInput)
+                cmpt120imageProjHelper.showInterface(img, "Choose from 1-7",generateMenu(state))
+
+        elif state["mode"] == "advanced": # check mode
+
+            if userInput == "1": # rotate left
+                print("Log: Performing " + advanced[int(userInput) - 1])
+                img = cmpt120imageManip.rotateLeft(img)
+                cmpt120imageProjHelper.showInterface(img, "Rotated Left",generateMenu(state))
+
+            elif userInput == "2": # rotate right
+                print("Log: Performing " + advanced[int(userInput) - 1])
+                img = cmpt120imageManip.rotateRight(img)
+                cmpt120imageProjHelper.showInterface(img, "Rotated Right",generateMenu(state))
+
+            elif userInput == "3": # double size
+                print("Log: Performing " + advanced[int(userInput) - 1])
+                img = cmpt120imageManip.doubleSize(img)
+                cmpt120imageProjHelper.showInterface(img, "Size Doubled",generateMenu(state))
+
+            elif userInput == "4": # half size
+                print("Log: Performing " + advanced[int(userInput) - 1])
+                img = cmpt120imageManip.halfSize(img)
+                cmpt120imageProjHelper.showInterface(img, "Size Halved",generateMenu(state))
+
+            elif userInput == "5": # locate fish
+                print("Log: Performing " + advanced[int(userInput) - 1])
+                box = cmpt120imageManip.locateFish(img)
+                cmpt120imageManip.makeGreenBox(img, box[0], box[1], box[2], box[3])
+                cmpt120imageProjHelper.showInterface(img, "Fish Located",generateMenu(state))
+
+            elif userInput == "6": # switch to basic
+                print("Log: Performing " + advanced[int(userInput) - 1])
+                state["mode"] = "basic"
+                cmpt120imageProjHelper.showInterface(img, "Basic Mode",generateMenu(state))
+
+            else: # unrecognized user input
+                print("Log: Unrecognized user input: " + userInput)
+                cmpt120imageProjHelper.showInterface(img, "Choose from 1-6",generateMenu(state))
+
+    else:  # unrecognized user input
+        print("Log: Unrecognized user input: " + userInput)
+    return img
+
+
+# *** DO NOT change any of the code below this point ***
+
+# use a dictionary to remember several state values of the application
+appStateValues = {
+    "mode": "basic",
+    "lastOpenFilename": "",
+    "lastSaveFilename": "",
+    "lastUserInput": ""
+}
+
+currentImg = cmpt120imageProjHelper.getBlackImage(300, 200)  # create a default 300 x 200 black image
+cmpt120imageProjHelper.showInterface(currentImg,"No Image",generateMenu(appStateValues))  # note how it is used
+
+# ***this is the event-loop of the application. Keep the remainder of the code unmodified***
+keepRunning = True
+# a while-loop getting events from pygame
+while keepRunning:
+    ### use the pygame event handling system ###
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            appStateValues["lastUserInput"] = pygame.key.name(event.key)
+            # prepare to quit the loop if user inputs "q" or "Q"
+            if appStateValues["lastUserInput"].upper() == "Q":
+                keepRunning = False
+            # otherwise let the helper function handle the input
+            else:
+                currentImg = handleUserInput(appStateValues, currentImg)
+        elif event.type == pygame.QUIT:  #another way to quit the program is to click the close botton
+            keepRunning = False
+
+# shutdown everything from the pygame package
+pygame.quit()
+
+print("Log: Program Quit")
